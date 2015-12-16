6a7c4823d9fd5c090322bfbcf0ef81e9b81401c7
J-DL <jean-damien.leau@edu.ece.fr>
2015-12-16 21:36:17 +0900

    A new model added and a the Result modele completed.

--- HYWebProject/app/Model/Result.php
+++ HYWebProject/app/Model/Result.php
@@ -4,7 +4,39 @@ App::uses('AppModel', 'Model');
 
 class Result extends AppModel
 {
-	
+	public function saveResult($teamList)
+	{
+		//We check how many results are already stored
+		$res = $this->find('all',array('fields' => array('id')));
+		
+		if(empty($res))
+		{
+			$number = 0;
+		}
+		else 
+		{
+			$number = count($res);
+		}
+		
+		$updatedList = array();
+		
+		for($i = 0; $i < count($teamList); $i++)
+		{
+			$this->create();
+			$newResult = array('id' => $i+$number+1,'prize' => $teamList[$i]['prize'], 'result' => $teamList[$i]['status']);
+			$this->save($newResult);
+			$this->clear();
+			
+			array_push($updatedList,array(	'id'		=> $teamList[$i]['id'],
+											'name' 		=> $teamList[$i]['name'],
+											'subject' 	=> $teamList[$i]['subject'],
+											'prize' 	=> $teamList[$i]['prize'],
+											'status' 	=> $teamList[$i]['status'],
+											'result_id'	=> $i+$number+1));
+		}
+		
+		return $updatedList;
+	}
 	
 	
 }
